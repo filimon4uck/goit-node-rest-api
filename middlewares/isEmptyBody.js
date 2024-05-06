55b372bb23637ef60b311ca0438bd7d2412cae1c
filimon4uck <filimon4uck.yura@gmail.com>
2024-05-06 19:06:27 +0200

    renamed isBodyValid added changed isEmptyBody,added result screenshots

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -3,7 +3,16 @@ const isEmptyBody = (req, res, next) => {
   const { length } = Object.keys(req.body);
 
   if (!length) {
-    return next(HttpError(400, "Body must have at least one key"));
+    switch (req.method) {
+      case "POST": {
+        return next(HttpError(400, "Body must have all fields"));
+      }
+      case "PUT": {
+        return next(HttpError(400, "Body must have at least one field"));
+        
+       }
+    }
+    
   }
   next();
 
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import contactsControllers from "../controllers/contactsControllers.js";
 import isEmptyBody from "../middlewares/isEmptyBody.js";
-import isBodyValid from "../middlewares/isBodyValid.js";
+import isValidBody from "../middlewares/isValidBody.js";
 
 
 const contactsRouter = express.Router();
@@ -12,8 +12,8 @@ contactsRouter.get("/:id", contactsControllers.getOneContact);
 
 contactsRouter.delete("/:id", contactsControllers.deleteContact);
 
-contactsRouter.post("/",isBodyValid.createValid ,contactsControllers.createContact);
+contactsRouter.post("/",isEmptyBody, isValidBody.createValid ,contactsControllers.createContact);
 
-contactsRouter.put("/:id",isEmptyBody,isBodyValid.updateValid, contactsControllers.updateContact);
+contactsRouter.put("/:id",isEmptyBody, isValidBody.updateValid, contactsControllers.updateContact);
 
 export default contactsRouter;
